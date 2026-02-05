fa9b502bc89e8f971dddac35a76a19592ca1f84f
Parth Sharma <psharma@singlestore.com>
2026-02-05 17:28:25 +0530

    changing the impl

--- src/metabase/driver/singlestore.clj
+++ src/metabase/driver/singlestore.clj
@@ -1,103 +1,15 @@
 (ns metabase.driver.singlestore
-  "SingleStore driver. Builds off of the MySQL driver since SingleStore is MySQL-compatible."
-  (:refer-clojure :exclude [some not-empty])
+  "SingleStore driver. Inherits from MySQL driver since SingleStore is MySQL-compatible."
   (:require
-   [clojure.java.jdbc :as jdbc]
-   [metabase.driver :as driver]
-   [metabase.driver.sql-jdbc.common :as sql-jdbc.common]
-   [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
-   [metabase.driver.sql.query-processor :as sql.qp]
-   [metabase.util.log :as log]
-   [metabase.util.performance :as perf :refer [some not-empty]])
-  (:import
-   (java.sql DatabaseMetaData ResultSet)))
+   [metabase.driver :as driver]))
 
 (set! *warn-on-reflection* true)
 
-;; Register SingleStore driver with MySQL as parent since SingleStore is MySQL-compatible
+;; Register SingleStore driver with MySQL as parent
+;; SingleStore is fully MySQL-compatible, so we inherit all behavior from MySQL
 (driver/register! :singlestore, :parent :mysql)
-
-(def ^:private ^:const min-supported-singlestore-version 7.3)
 
 (defmethod driver/display-name :singlestore [_] "SingleStore")
 
-;; Feature support - inherits most from MySQL, but may need adjustments
-(doseq [[feature supported?] {;; SingleStore-specific features
-                              :window-functions/offset true  ;; SingleStore supports this unlike MySQL
-                              :full-join              true   ;; SingleStore supports full joins
-                              }]
-  (defmethod driver/database-supports? [:singlestore feature] [_driver _feature _db] supported?))
-
-;;; ------------------------------------------ Connection Details ------------------------------------------
-
-(defmethod sql-jdbc.conn/connection-details->spec :singlestore
-  [_ {:keys [host port db]
-      :or   {host "localhost", port 3306, db ""}
-      :as   details}]
-  (-> (merge {:classname                     "com.mysql.cj.jdbc.Driver"
-              :subprotocol                   "mysql"
-              :subname                       (str "//" host ":" port "/" db)
-              :useUnicode                    true
-              :characterEncoding             "UTF8"
-              :characterSetResults           "UTF8"
-              ;; SingleStore-specific optimizations
-              :connectionAttributes          "program_name:Metabase"
-              ;; Timezone handling
-              :connectionTimeZone            "UTC"
-              :forceConnectionTimeZoneToSession true}
-             (dissoc details :host :port :db :ssl))
-      (sql-jdbc.common/handle-additional-options details)))
-
-;;; ------------------------------------------ Database Info ------------------------------------------
-
-(defn- singlestore-version
-  "Fetch the version of the SingleStore database."
-  [database]
-  (let [spec (sql-jdbc.conn/db->pooled-connection-spec database)]
-    (jdbc/with-db-connection [conn spec]
-      (-> (.getConnection conn)
-          .getMetaData
-          .getDatabaseProductVersion))))
-
-(defmethod driver/dbms-version :singlestore
-  [driver database]
-  (let [version-str (singlestore-version database)]
-    ;; Parse version string like "8.0.123" or "memsql-8.0.123"
-    (when-let [version-match (re-find #"(\d+\.\d+)" version-str)]
-      {:version version-str
-       :semantic-version (second version-match)})))
-
-;;; ------------------------------------------ Sync ------------------------------------------
-
-;; SingleStore has both columnstore and rowstore tables
-;; For now, we inherit all sync behavior from MySQL
-;; TODO: Add SingleStore-specific table metadata like storage type (columnstore/rowstore)
-;; This would require a query like:
-;; SELECT STORAGE_TYPE FROM information_schema.TABLES WHERE TABLE_NAME = ?
-
-;;; ------------------------------------------ Query Processor ------------------------------------------
-
-;; SingleStore-specific SQL generation adjustments if needed
-;; Most SQL generation is inherited from MySQL
-
-;; Example: Override if SingleStore has different function syntax
-#_(defmethod sql.qp/->honeysql [:singlestore :some-function]
-    [driver [_ arg]]
-    (hsql/call :singlestore_function arg))
-
-;;; ------------------------------------------ Query Optimization Hints ------------------------------------------
-
-;; SingleStore supports optimizer hints that might be useful for query optimization
-;; Example: /*+ USE_HASH_JOIN */ 
-;; This can be implemented when needed
-
-(comment
-  ;; Example of adding optimizer hints
-  (defmethod sql.qp/apply-top-level-clause [:singlestore :singlestore/optimizer-hint]
-    [driver _ honeysql-query [_ hint]]
-    (update honeysql-query :modifiers (fnil conj []) hint)))
-
-;;; ------------------------------------------ Database Creation (for testing) ------------------------------------------
-
-;; Note: Database creation/management methods are typically defined in test namespaces
-;; See metabase.test.data.singlestore for test-specific implementations
+;; All other functionality is inherited from MySQL driver
+;; Override specific methods here when SingleStore-specific behavior is needed
--- test/metabase/test/data/singlestore.clj
+++ test/metabase/test/data/singlestore.clj
@@ -1,32 +1,11 @@
 (ns metabase.test.data.singlestore
   "Code for creating / destroying a SingleStore database from a `DatabaseDefinition`.
-   SingleStore is MySQL-compatible, so we inherit most behavior from MySQL."
+   SingleStore is MySQL-compatible, so we inherit all behavior from MySQL."
   (:require
-   [clojure.java.jdbc :as jdbc]
-   [clojure.string :as str]
-   [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
    [metabase.test.data.interface :as tx]
-   [metabase.test.data.sql :as sql.tx]
-   [metabase.test.data.sql-jdbc :as sql-jdbc.tx]
-   [metabase.test.data.sql-jdbc.load-data :as load-data]))
+   [metabase.test.data.sql :as sql.tx]))
 
-(sql-jdbc.tx/add-test-extensions! :singlestore)
-
-;;; ------------------------------------------ Field Type Mappings ------------------------------------------
-
-;; SingleStore supports the same data types as MySQL
-(doseq [[base-type database-type] {:type/BigInteger     "BIGINT"
-                                   :type/Boolean        "BOOLEAN"
-                                   :type/Date           "DATE"
-                                   :type/DateTime       "DATETIME(3)"
-                                   :type/DateTimeWithTZ "TIMESTAMP(3) DEFAULT '1970-01-01 00:00:01'"
-                                   :type/Decimal        "DECIMAL"
-                                   :type/Float          "DOUBLE"
-                                   :type/Integer        "INTEGER"
-                                   :type/JSON           "JSON"
-                                   :type/Text           "TEXT"
-                                   :type/Time           "TIME(3)"}]
-  (defmethod sql.tx/field-base-type->sql-type [:singlestore base-type] [_ _] database-type))
+;; SingleStore inherits test extensions from MySQL (its parent), so no need to add them again
 
 ;;; ------------------------------------------ Connection Details ------------------------------------------
 
@@ -41,99 +20,16 @@
    (when (= context :db)
      {:db database-name})))
 
-;;; ------------------------------------------ Database Creation ------------------------------------------
+;;; ------------------------------------------ Field Type Mappings ------------------------------------------
 
-(defmethod sql.tx/create-db-sql :singlestore
-  [_ {:keys [database-name]}]
-  ;; SingleStore specific: You can optionally specify PARTITIONS for the database
-  ;; For now, we'll use default partitioning
-  (format "CREATE DATABASE IF NOT EXISTS %s;" database-name))
+;; SingleStore supports the same data types as MySQL
+;; Inherit all field type mappings from MySQL by dispatching to parent
+(doseq [base-type [:type/BigInteger :type/Boolean :type/Date :type/DateTime
+                   :type/DateTimeWithTZ :type/Decimal :type/Float :type/Integer
+                   :type/JSON :type/Text :type/Time]]
+  (defmethod sql.tx/field-base-type->sql-type [:singlestore base-type]
+    [driver base-type-kw]
+    ((get-method sql.tx/field-base-type->sql-type [:mysql base-type-kw]) driver base-type-kw)))
 
-(defmethod sql.tx/drop-db-if-exists-sql :singlestore
-  [_ {:keys [database-name]}]
-  (format "DROP DATABASE IF EXISTS %s;" database-name))
-
-;;; ------------------------------------------ Table Creation ------------------------------------------
-
-;; SingleStore supports both ROWSTORE and COLUMNSTORE tables
-;; By default, we'll create COLUMNSTORE tables for analytics workloads
-;; This can be overridden by adding table-level options
-
-(defmethod sql.tx/create-table-sql :singlestore
-  [driver {:keys [database-name], :as dbdef} {:keys [table-name field-definitions table-options]}]
-  (let [field-sql (map (fn [{:keys [field-name base-type field-options]}]
-                         (str (sql.tx/qualify-and-quote driver field-name)
-                              " "
-                              (sql.tx/field-base-type->sql-type driver base-type)
-                              (when-let [default (:default field-options)]
-                                (str " DEFAULT " default))))
-                       field-definitions)
-        ;; SingleStore specific: Add COLUMNSTORE by default unless specified otherwise
-        storage-type (get table-options :storage-type "COLUMNSTORE")
-        create-sql (format "CREATE %s TABLE %s.%s (%s)"
-                           storage-type
-                           (sql.tx/qualify-and-quote driver database-name)
-                           (sql.tx/qualify-and-quote driver table-name)
-                           (str/join ", " field-sql))]
-    create-sql))
-
-;;; ------------------------------------------ Data Loading ------------------------------------------
-
-;; Inherit data loading from SQL-JDBC
-;; SingleStore supports standard INSERT statements like MySQL
-
-(defmethod load-data/load-data! :singlestore
-  [& args]
-  (apply load-data/load-data-all-at-once! args))
-
-(defmethod load-data/do-insert! :singlestore
-  [driver spec table-identifier row-or-rows]
-  (load-data/do-insert-with-jdbc! driver spec table-identifier row-or-rows))
-
-;;; ------------------------------------------ Aggregate Column Info ------------------------------------------
-
-(defmethod tx/aggregate-column-info :singlestore
-  ([driver ag-type]
-   ((get-method tx/aggregate-column-info ::tx/test-extensions) driver ag-type))
-
-  ([driver ag-type field]
-   ((get-method tx/aggregate-column-info ::tx/test-extensions) driver ag-type field)))
-
-;;; ------------------------------------------ Role Management (for testing impersonation) ------------------------------------------
-
-;; SingleStore supports MySQL-compatible role management
-;; This is similar to MySQL's implementation
-
-(defn grant-table-perms-to-roles!
-  [driver details roles]
-  (let [spec (sql-jdbc.conn/connection-details->spec driver details)]
-    (doseq [[role-name table-perms] roles]
-      (let [role-name (sql.tx/qualify-and-quote driver role-name)]
-        (doseq [[table-name perms] table-perms]
-          (let [columns (:columns perms)
-                select-cols (str/join ", " (map #(sql.tx/qualify-and-quote driver %) columns))
-                grant-stmt (if (not= select-cols "")
-                             (format "GRANT SELECT (%s) ON %s TO %s" select-cols table-name role-name)
-                             (format "GRANT SELECT ON %s TO %s" table-name role-name))]
-            (jdbc/execute! spec [grant-stmt] {:transaction? false})))))))
-
-(defmethod tx/create-and-grant-roles! :singlestore
-  [driver details roles user-name default-role]
-  (let [spec (sql-jdbc.conn/connection-details->spec driver details)]
-    (doseq [statement [(format "DROP USER IF EXISTS '%s'@'%%';" user-name)
-                       (format "CREATE USER '%s'@'%%' IDENTIFIED BY '';" user-name)
-                       (format "DROP ROLE IF EXISTS %s;" default-role)
-                       (format "CREATE ROLE %s;" default-role)
-                       (format "GRANT SELECT ON *.* TO %s;" default-role)
-                       (format "GRANT %s TO %s;" default-role user-name)
-                       (format "SET DEFAULT ROLE %s TO %s;" default-role user-name)]]
-      (jdbc/execute! spec [statement]))
-    (sql-jdbc.tx/drop-if-exists-and-create-roles! driver details roles)
-    (grant-table-perms-to-roles! driver details roles)
-    (sql-jdbc.tx/grant-roles-to-user! driver details roles user-name)))
-
-;;; ------------------------------------------ Test Data Specific ------------------------------------------
-
-;; Any SingleStore-specific test data adjustments can go here
-;; For example, if certain test datasets need special handling for SingleStore's
-;; distributed architecture or storage types
+;;; All other test functionality is inherited from MySQL through parent driver
+;;; Override specific methods here when SingleStore-specific test behavior is needed
